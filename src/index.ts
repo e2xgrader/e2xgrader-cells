58ecd6543a08e005bd19343cd33fda3569eca5b2
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-02-26 16:25:40 +0100

    Export @e2xgrader/cells extension

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,43 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { NotebookPanel } from '@jupyterlab/notebook';
+import { IEditorServices } from '@jupyterlab/codeeditor';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { E2XContentFactory } from './factory';
 
 /**
  * Initialization data for the @e2xgrader/cells extension.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@e2xgrader/cells:plugin',
+const plugin: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
+  id: '@e2xgrader/cells:factory',
   description: 'A JupyterLab extension.',
   autoStart: true,
+  requires: [IEditorServices],
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  provides: NotebookPanel.IContentFactory,
+  activate: async (
+    _app: JupyterFrontEnd,
+    editorServices: IEditorServices,
+    settingRegistry: ISettingRegistry | null
+  ) => {
     console.log('JupyterLab extension @e2xgrader/cells is activated!');
+    const editorFactory = editorServices.factoryService.newInlineEditor;
+    let contentFactory: E2XContentFactory;
 
     if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('@e2xgrader/cells settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for @e2xgrader/cells.', reason);
-        });
+      try {
+        const settings = await settingRegistry.load(plugin.id);
+        contentFactory = new E2XContentFactory({ editorFactory }, settings);
+      } catch (reason) {
+        console.error('Failed to load settings for @e2xgrader/cells', reason);
+        contentFactory = new E2XContentFactory({ editorFactory });
+      }
+    } else {
+      contentFactory = new E2XContentFactory({ editorFactory });
     }
+    return contentFactory;
   }
 };
 
